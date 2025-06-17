dac855421828ac0b55c04a5cf3556bafc9b4af1d
closetdog <101305067+closetdog@users.noreply.github.com>
2025-06-17 15:32:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def generate_sop_doc(data):
         pPr.append(pBdr)
 
     def add_paragraph(text, bold=False, size=11, spacing=1.5, indent=None):
-        para = doc.add_paragraph()
+    para = doc.add_paragraph()
     para.paragraph_format.space_before = Pt(0)
     para.paragraph_format.space_after = Pt(0)
         run = para.add_run(text)
