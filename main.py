9beb8b4bd5658f3f9b089cd1248acd1390c60bf0
closetdog <101305067+closetdog@users.noreply.github.com>
2025-06-17 19:00:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -138,11 +138,12 @@ def generate_sop_doc(data):
                     para = doc.add_paragraph()
                     para.paragraph_format.space_before = Pt(0)
                     para.paragraph_format.space_after = Pt(0)
-                    run1 = para.add_run(f"{label}: ")
+                    run1 = para.add_run(f"{label}. ")
                     run1.bold = True
                     run1.font.size = Pt(11)
                     run1.font.color.rgb = RGBColor(0, 0, 0)
                     run2 = para.add_run(value)
+                    run2.bold = False
                     para.paragraph_format.line_spacing = 1.0 if label in ["Scope", "Role", "Output", "Interaction"] else 1.5
                     run2.font.size = Pt(11)
                     run2.font.color.rgb = RGBColor(0, 0, 0)
